e0a31d4e95d192ed913eb500f36f6c1d6fdf981c
yuanbao97 <316261393@qq.com>
2021-01-25 15:39:06 +0800

    6.2.1 存数据

--- app/src/main/java/com/example/filepersistencetest/activity/MainActivity.java
+++ app/src/main/java/com/example/filepersistencetest/activity/MainActivity.java
@@ -1,17 +1,56 @@
 package com.example.filepersistencetest.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.filepersistencetest.R;
 import com.example.filepersistencetest.framework.BaseActivity;
 
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.save_edit_view)
+    EditText saveEditView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputText = saveEditView.getText().toString();
+        save(inputText);
+    }
+
+    public void save(String inputText) {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(writer != null) {
+                    writer.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
