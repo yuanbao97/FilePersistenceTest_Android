6e70db3a5ef455c5156b3f3924d648dbe2d7099b
yuanbao97 <316261393@qq.com>
2021-01-25 16:35:32 +0800

    6.2.2 取数据

--- app/src/main/java/com/example/filepersistencetest/activity/MainActivity.java
+++ app/src/main/java/com/example/filepersistencetest/activity/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.filepersistencetest.activity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.filepersistencetest.R;
 import com.example.filepersistencetest.framework.BaseActivity;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import butterknife.BindView;
@@ -25,22 +30,28 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        String inputText = load();
+        if(!TextUtils.isEmpty(inputText)) {
+            saveEditView.setText(inputText);
+            saveEditView.setSelection(inputText.length());
+            Toast.makeText(this, "加载成功", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        String inputText = saveEditView.getText().toString();
-        save(inputText);
+        String outputText = saveEditView.getText().toString();
+        save(outputText);
     }
 
-    public void save(String inputText) {
+    public void save(String outputText) {
         FileOutputStream out = null;
         BufferedWriter writer = null;
         try {
             out = openFileOutput("data", Context.MODE_PRIVATE);
             writer = new BufferedWriter(new OutputStreamWriter(out));
-            writer.write(inputText);
+            writer.write(outputText);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -52,5 +63,30 @@ public class MainActivity extends BaseActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public String load() {
+        StringBuilder content = new StringBuilder();
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return content.toString();
     }
 }
